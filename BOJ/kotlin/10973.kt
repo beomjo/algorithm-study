cefd1d4f397e30a51dc039c4da4dd6a162db1719
beomjo <qjatjr1108@gmail.com>
2021-04-11 20:39:22 +0900

    feat: PS BOJ 10973. 이전 순열

---
+++ BOJ/kotlin/10973.kt
@@ -0,0 +1,30 @@
+fun main() {
+    val n = readLine()!!.toInt()
+    val a = readLine()!!.split(" ").map { it.toInt() }.toMutableList()
+
+    if (isNextPermutation(a)) {
+        println(a.joinToString(" "))
+    } else {
+        println(-1)
+    }
+}
+
+fun isNextPermutation(a: MutableList<Int>): Boolean {
+    var i = (1 until a.size).findLast { a[it] < a[it - 1] } ?: return false
+    var j = (i until a.size).findLast { a[it] < a[i - 1] } ?: return false
+    a.swap(i - 1, j)
+
+    j = a.size - 1
+    while (i < j) {
+        a.swap(i, j)
+        i++
+        j--
+    }
+    return true
+}
+
+fun <T> MutableList<T>.swap(i: Int, j: Int) {
+    val temp = this[i]
+    this[i] = this[j]
+    this[j] = temp
+}
