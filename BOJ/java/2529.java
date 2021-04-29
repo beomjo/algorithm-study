0be5f3e15dbde5858201fef2c30faa9aace2bfb8
beomjo <qjatjr1108@gmail.com>
2021-04-30 03:08:31 +0900

    feat: PS BOJ 2529. 부등호

---
+++ BOJ/java/2529.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Main {
+
+    static String[] arr;
+    static boolean[] number = new boolean[10];
+    static List<String> answer;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.next());
+        arr = new String[n];
+        answer = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.next();
+        }
+
+        dfs(n, 0, "");
+        System.out.println(answer.get(answer.size() - 1));
+        System.out.println(answer.get(0));
+    }
+
+    private static void dfs(int n, int index, String s) {
+
+        String result;
+
+        if (index == n + 1) {
+            if (check(n, s))
+                answer.add(s);
+            return;
+        }
+
+        for (int i = 0; i <= 9; i++) {
+
+            if (number[i])
+                continue;
+
+            result = s + i;
+
+            number[i] = true;
+            dfs(n, index + 1, result);
+            number[i] = false;
+        }
+    }
+
+    private static boolean check(int n, String answer) {
+        for (int i = 0; i < n; i++) {
+            char[] chars = answer.toCharArray();
+            if (arr[i].equals(">")) {
+                if (chars[i] > chars[i + 1])
+                    continue;
+                return false;
+            }
+            if (arr[i].equals("<")) {
+                if (chars[i] < chars[i + 1])
+                    continue;
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ BOJ/kotlin/2529.kt
@@ -0,0 +1,33 @@
+private lateinit var arr:Array<String>
+private val list= arrayListOf<String>()
+private val visited=BooleanArray(10)
+fun main() {
+    val br=System.`in`.bufferedReader()
+    val n=br.readLine().toInt()
+    arr=br.readLine().split(" ").toTypedArray()
+    dfs(0,"",n)
+    list.sort()
+    println(list[list.size-1])
+    println(list[0])
+}
+private fun dfs(idx:Int,number:String,n:Int){
+    if (idx==n+1){
+        list.add(number)
+        return
+    }
+    for(i in 0 ..9){
+        if(visited[i]) continue
+        if(idx==0 || check(number[idx-1],(i + '0'.toInt()).toChar(),arr[idx-1])){
+            visited[i]=true
+            dfs(idx+1,number+i.toString(),n)
+            visited[i]=false
+        }
+    }
+}
+private fun check(x:Char,y:Char,operation:String):Boolean{
+    return when {
+        operation=="<" && x>y -> false
+        operation==">" && x<y -> false
+        else -> true
+    }
+}
